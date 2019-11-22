c1edb7873bf4522ae0d201320ca76b67f0083d49
Pavel Sergeev <pavel.sergeev@jetbrains.com>
2019-11-22 18:08:16 +0300

    make.sh now works on linux

--- make.sh
+++ make.sh
@@ -2,7 +2,13 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/"
 
-mkdir -p native/build/darwin
-cd native/build/darwin
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  mkdir -p native/build/linux-x86-64
+  cd native/build/linux-x86-64
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  mkdir -p native/build/darwin
+  cd native/build/darwin
+fi
+
 cmake ../..
 make
